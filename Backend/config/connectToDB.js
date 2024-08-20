f83a333edc057c6e559904bea52222b1928343c8
Ami Nice <amimakeup@gmail.com>
2024-08-21 00:29:50 +0300

    Update connectToDB.js

--- Backend/config/connectToDB.js
+++ Backend/config/connectToDB.js
@@ -1,17 +1,11 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectToDb = () => {
-    mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
+    mongoose.connect("mongodb://127.0.0.1:27017/fetch-metadata").then(()=>{
         console.log("Connected to the database");
-    }).catch(err => {
-        console.error("Database connection error:", err);
-    });
+    })
 };
 
-module.exports=connectToDb
+module.exports = connectToDb;
 
 
